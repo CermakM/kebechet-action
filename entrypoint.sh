af1183ca3598978973063d50638596ba8f001ba1
Marek Cermak <macermak@redhat.com>
2019-09-06 21:32:38 +0200

    By pass the SSH problems and base64 decode
    modified:   build/Dockerfile
    modified:   entrypoint.sh
    
    Test permissions
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -l
 
+# The git_ssh_command helps the server by pass the Host key checking while connecting to github.
+export GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
 # Set the SSH secret
-mkdir -p $HOME/.ssh/ && echo "${KEBECHET_SSH_PRIVATE_KEY}" > $HOME/.ssh/id_rsa
+mkdir -p $HOME/.ssh/
+echo "${KEBECHET_SSH_PRIVATE_KEY}" | base64 --decode > $HOME/.ssh/id_rsa && chmod 400 $HOME/.ssh/id_rsa
+
+# Test permissions
+ssh -vT git@github.com
 
 # Run Kebechet
 echo "\nRunning Kebechet ...\n"
