f9b81a9709f2121b4b2dbe7c35f1b7d8bbc9d749
Marek Cermak <macermak@redhat.com>
2019-09-07 10:07:47 +0200

    Small esthetic changes
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export KEBECHET_REPO_URL="https://github.com/${GITHUB_REPOSITORY}"
 export KEBECHET_TOKEN="${GITHUB_TOKEN}"
 export SSH_HOME_DIRECTORY=/root
 
-decode() {
+_decode() {
     local decoded="$(echo \"$1\" | base64 -i -w 0 --decode)"
     if [ $? -ne 0 ] ; then
         exit 1;
@@ -14,10 +14,10 @@ decode() {
     >&1 echo "$decoded"
 }
 
-prep() {
+_prep() {
     # Set the SSH secret
     mkdir -p ${SSH_HOME_DIRECTORY}/.ssh/
-    decode "${KEBECHET_SSH_PRIVATE_KEY}" > ${SSH_HOME_DIRECTORY}/.ssh/id_rsa && chmod 0400 ${SSH_HOME_DIRECTORY}/.ssh/id_rsa
+    _decode "${KEBECHET_SSH_PRIVATE_KEY}" > ${SSH_HOME_DIRECTORY}/.ssh/id_rsa && chmod 0400 ${SSH_HOME_DIRECTORY}/.ssh/id_rsa
 
     # Test permissions
     # Ignore exit code as exit status 1 is returned when no terminal is present, serves for debugging purposes
@@ -25,7 +25,7 @@ prep() {
 }
 
 main() {
-    prep || exit 1
+    _prep || exit 1
 
     # Run Kebechet
     echo "\nRunning Kebechet ...\n"
